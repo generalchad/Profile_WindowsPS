96b0b72fa44cc18910570df0d9ac33283880c1ce
genchadt <git+github@timothywb.com>
2026-04-12 21:36:28 -0400

    Encapsulate M365 updater so it doesn't run on shell start.

--- Functions/Update-M365.ps1
+++ Functions/Update-M365.ps1
@@ -1,40 +1,42 @@
-<#
-.SYNOPSIS
-    Launches the Office update process.
+function Update-M365 {
+    <#
+    .SYNOPSIS
+        Launches the Office update process.
 
-.DESCRIPTION
-    Launches the Office Click-to-Run update installer.
+    .DESCRIPTION
+        Launches the Office Click-to-Run update installer.
 
-.PARAMETER C2RClientPath
-    The path to the Office Click-to-Run client executable.
-    By default, this is set to "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe".
+    .PARAMETER C2RClientPath
+        The path to the Office Click-to-Run client executable.
+        By default, this is set to "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe".
 
-.PARAMETER C2R_args
-    The arguments to pass to the Office Click-to-Run client executable.
-    By default, this is set to "/update user". 
-    Documentation is seemingly not available for the C2R client so it's not recommended to change.
-#>
+    .PARAMETER C2R_args
+        The arguments to pass to the Office Click-to-Run client executable.
+        By default, this is set to "/update user".
+        Documentation is seemingly not available for the C2R client so it's not recommended to change.
+    #>
 
-[CmdletBinding()]
-param (
-    [Parameter(Position = 0)]
-    [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
-    [string]$C2RClientPath = "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe",
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+        [string]$C2RClientPath = "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe",
 
-    [Parameter(Position = 1)]
-    [string]$C2R_args = "/update user"
-)
+        [Parameter(Position = 1)]
+        [string]$C2R_args = "/update user"
+    )
 
-$C2RClientName = [System.IO.Path]::GetFileName($C2RClientPath)
+    $C2RClientName = [System.IO.Path]::GetFileName($C2RClientPath)
 
-try {
-    Write-Verbose "Attemping to launch $C2RClientName..."
-    Write-Verbose "C2R Command: $C2RClientPath $C2R_args"
-    Start-Process -FilePath $C2RClientPath -ArgumentList $C2R_args
-}
-catch {
-    Write-Warning "Failed to start Office update process: $_"
-    Write-Debug "C2RClientPath: $C2RClientPath"
-    Write-Debug "C2R_args: $C2R_args"
-    Write-Debug "Line: $($_.InvocationInfo.ScriptLineNumber)"
+    try {
+        Write-Verbose "Attemping to launch $C2RClientName..."
+        Write-Verbose "C2R Command: $C2RClientPath $C2R_args"
+        Start-Process -FilePath $C2RClientPath -ArgumentList $C2R_args
+    }
+    catch {
+        Write-Warning "Failed to start Office update process: $_"
+        Write-Debug "C2RClientPath: $C2RClientPath"
+        Write-Debug "C2R_args: $C2R_args"
+        Write-Debug "Line: $($_.InvocationInfo.ScriptLineNumber)"
+    }
 }
